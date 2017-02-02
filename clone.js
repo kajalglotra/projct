fe88fed11e67f8fe2378963323f8413725ee997d
satyametch <satya@excellencetechnologies.in>
2017-02-02 18:10:59 +0530

    selector

---
+++ clone.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".add").click(function(){
+        $(".inner").append("<div id='abc'><input type='search'><button>browse</button>&nbsp<select><option>secondary image</option></select>&nbsp&nbsp<a id='remove'>remove</a></div></div>");
+     
+ });
+      $(document).on("click","#remove",function(){
+
+
+        $("#abc").remove();
+    });
+     
+});
