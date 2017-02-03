82073057154a7357beac40971ea142e7f5a976fc
kajalglotra <bulbulkjl@gmail.com>
2017-02-03 10:44:12 +0530

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
      $(".add").click(function(){
-        $(".inner").append("<div id='abc'><input type='search'><button>browse</button>&nbsp<select><option>secondary image</option></select>&nbsp&nbsp<a id='remove'>remove</a></div></div>");
+        $(".inner").append("<div id='removal'><input type='search'><button>browse</button>&nbsp<select><option>secondary image</option></select>&nbsp&nbsp<a id='remove'>remove</a></div></div>");
     });
   $(document).on("click","#remove",function(){
-    $("#abc").remove();
+    $("#removal").remove();
   });
 });
