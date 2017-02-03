83abf5399ac0da6c88664c25ed9f55a042057e5e
kajalglotra <bulbulkjl@gmail.com>
2017-02-03 10:26:46 +0530

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
-    $(".add").click(function(){
+     $(".add").click(function(){
         $(".inner").append("<div id='abc'><input type='search'><button>browse</button>&nbsp<select><option>secondary image</option></select>&nbsp&nbsp<a id='remove'>remove</a></div></div>");
-     
- });
-      $(document).on("click","#remove",function(){
-
-
-        $("#abc").remove();
     });
-     
+  $(document).on("click","#remove",function(){
+    $("#abc").remove();
+  });
 });
